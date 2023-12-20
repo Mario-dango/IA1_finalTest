550c30a228dab3d4ef6c6ed2d6da4b2c7ef3eb42
Mario-dango <mp.robots@gmail.com>
2023-12-20 01:19:36 -0300

    se cargaron imagenes primitivas

--- examples/mhu.py
+++ examples/mhu.py
@@ -2,20 +2,42 @@ import cv2
 import numpy as np
 
 #Leo la imagen de diversas figuras
-# figuras = cv2.imread("../resources/images/formas.png")
-figuras = cv2.imread("../resources/pictures/tornillo01.jpeg")
+figuras = cv2.imread("../resources/images/formas1.png")
+figuras = cv2.imread("../resources/images/formasP.png")
+# figuras = cv2.imread("../resources/pictures/tornillo01.jpeg")
 # figuras = cv2.imread("../resources/dataset/internet/arandela06.jpg")
 #Creo una imagen apartir de una transformación a grices de la imagen original
 grices = cv2.cvtColor(figuras, cv2.COLOR_BGR2GRAY)
 #
 imgCanny = cv2.Canny(figuras, 10, 50)
+image_filtered = cv2.GaussianBlur(figuras, (5, 5), 0)
 
-imgCanny
-ret,th = cv2.threshold(grices,100,255,cv2.THRESH_BINARY_INV)
+# Encuentra los contornos en la imagen filtrada por Canny
+contours, _ = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(imgCanny, contours, -1, (0,255,0),3)
+
+# Calcula los momentos de Hu para el primer contorno encontrado
+for c in range(len(contours)):
+    moments = cv2.moments(contours[c])
+    hu_moments = cv2.HuMoments(moments)
+    print("Hu Moments de la imagen filtrada por Canny:")
+    for i in range(0, 7):
+        print(f"Hu Moment {i}: {hu_moments[i]}")
+
+
+
+_,th = cv2.threshold(grices,100,255,cv2.THRESH_BINARY_INV)
 #Consigo los contornos ordenandolos en cantidad y por jerarquia
-contornos,jerarquia = cv2.findContours(th,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+contornos,_ = cv2.findContours(th,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 #dibujo los contornos
 cv2.drawContours(figuras, contornos, -1, (0,255,0),3)
+
+# contG,_ = cv2.findContours(image_filtered,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+# contC,_ = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+
+cv2.imshow("imagen filtro gausiano", image_filtered)
+cv2.imshow("imagen filtro Canny", imgCanny)
 
 #por cada elemento dentro de  la cantidad de contornos
 for conts in range (len(contornos)):     
@@ -31,6 +53,19 @@ for conts in range (len(contornos)):
     print(cX);print(cY)
     #Dibujo un punto en donde corresponde la coordenada de su centroide
     cv2.circle(figuras,(cX,cY),5,(130,130,130),-1)
+    #elipse
+    if len(contornos[conts]) > 5:            
+        elip = cv2.fitEllipse(contornos[conts])
+        print("El valor de elipse es: ", elip)
+        # Calcular los ejes mayor y menor de la elipse ajustada
+        major_axis = max(elip[1])
+        minor_axis = min(elip[1])
+        # Calcular la excentricidad
+        excentricity = major_axis / minor_axis
+
+        # Imprimir el valor de excentricidad
+        print("Excentricidad:", excentricity)
+        
     
     #Calculo la longitud de curva del contorno
     per = cv2.arcLength(cnt, True)
